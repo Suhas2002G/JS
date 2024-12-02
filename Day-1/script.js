94aea661d709af68260c43471cacee157773acde
Suhas Gondukupi <suhas8838@gmail.com>
2024-12-02 12:27:57 +0530

    js

---
+++ Day-1/script.js
@@ -0,0 +1,16 @@
+function display(){
+    // alert('Welcome')
+
+    // after clicking on sunscribe welcome to pune --> welcome to mumbai
+    let headingElement=document.querySelector('.js-my-heading')
+    headingElement.innerHTML='Welcome to Mumbai'
+
+}
+
+function sum(){
+    let a=10
+    let b=20
+    let c= a+b
+   console.log(c)       // to display on console 
+    alert(a+b)
+}
---
+++ NOTES/main.py
@@ -0,0 +1,45 @@
+# n=int(input("Enter a num  : "))
+# if n % 2 == 0:
+#     print("Even")
+#     print(n**2)
+
+# else:
+#     print('Odd')
+#     print(n**3)
+
+
+
+# for i in range(50):
+#     print('suhas')
+
+# s=0
+# for i in range(1,6,1):
+#     s=s+i
+# print(s)
+
+
+
+
+# prime number or not 
+# n=int(input("Enter a num  : "))
+# for i in range(2, n):  
+#     if n % i == 0:
+#         a = 1  # Not prime
+#         break
+#     else:  
+#         a = 0  #prime
+
+# if a == 0:
+#     print('prime')
+# else:
+#     print('not prime')
+
+
+n=5
+i=1
+sum=0
+while (i<=n):
+    sum=sum+i
+    i=i+1
+
+print(sum)
